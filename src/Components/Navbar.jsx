664610e3539eb58e7262db602cfeb049e1380566
Nikil kumar <kumarnikil220@gmail.com>
2023-01-23 10:56:54 +0530

    Now you can add items to cart

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Navbar = () => {
+    //useSelector hook under the hook data ko subscribe karke rakhta hai mean agar value change 
+    //hoti hai to ye humko changed values he dega 
+    // ye jo state le rha hai arrow fun mai ye hamari puri application ke states hai na kii 
+    //sirf cart kii 
+    // ye hook jo function liye hai isme  he aapko batana hota hai konsi state ko aap log
+    //subscribe karke rakhna chahte hai 
+    const Items = useSelector((state) => state.cart);
+
     return (
         <div>
             <span className='logo' style={{ display: 'flex', alignItems: 'center', justifyContent: "flex-start" }}>REDUX STORE</span>
@@ -12,7 +21,7 @@ const Navbar = () => {
             }}>
                 <Link className='navLink' to="/">Home</Link>
                 <Link className='navLink' to="/cart">Cart</Link>
-                <span className='cartCount'>Cart Items:0</span>
+                <span className='cartCount'>Cart Items:{Items.length}</span>
             </div>
         </div>
     )
