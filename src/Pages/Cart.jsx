c73c14da37ed099e8cfccab122597df926dc79fe
Nikil kumar <kumarnikil220@gmail.com>
2023-01-23 13:29:58 +0530

    Now user can remove items from cart

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
-
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';  //to use the store data
+import { remove } from '../store/cartSlice';
 const Cart = () => {
+  //avv remove karenge to ek action ko dispatch bhi to karna padega 
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.cart);  //Subscribe cart state 
+  //receiving product id
+  const handleRemove = (productId) => {
+    //dispatch mai humko ek action dena hota hai with payload 
+    dispatch(remove(productId))
+  }
   return (
     <div>
-         I'm Cart
+      <h3>Cart</h3>
+      <div className='cartWapper'>
+        {
+          products.map((product) => (
+            <div key={product.id} className="cartCard">
+              <img src={product.image} alt="" />
+              <h5>{product.title}</h5>
+              <h5>{product.price}</h5>
+              <button
+                onClick={() => handleRemove(product.id)} //id ke through hum particular product ko remove karenge 
+                className="btn">
+                Remove
+              </button>
+            </div>
+
+          ))}
+
+      </div>
     </div>
   )
 }
 
-export default Cart
-   
+export default Cart;
