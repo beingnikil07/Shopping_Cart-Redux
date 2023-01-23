39e7d5bfa7a4c78f3a278ce3339b1dd07ab5905f
Nikil kumar <kumarnikil220@gmail.com>
2023-01-23 09:59:37 +0530

    components is wrapped to use redux store state

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home.jsx';
 import Cart from './Pages/Cart.jsx';
 import Navbar from './Components/Navbar';
+import { Provider } from 'react-redux';
+import store from './store/store';   //importing redux store 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/cart" element={<Cart />}></Route>
-        </Routes>
-      </Router>
+    <div>
+      <Provider store={store}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
+          </Routes>
+        </Router>
+      </Provider>
     </div>
   );
 }
